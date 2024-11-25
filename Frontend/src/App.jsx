e6473a285b715aacd79d92083b8e1019e8229511
Ankit Sharma <ankitsharma11876@gmail.com>
2024-11-25 18:20:22 +0530

    Book Store Project build on mern stack

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Components/Banner"
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Banner.jsx
@@ -0,0 +1,20 @@
+const Banner = () => {
+  return (
+         <>
+         <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+
+          <div className="w-full  md:w-1/2 mt-12 md:mt-32">
+             <div className="space-y-12">
+
+            <h1 className="text-4xl font-bold">Hello,Welcomes here to learn something <span className="text-yellow-500">new everyday!!</span></h1>
+            <p className="text-xl ">Discover a vast collection of books across genres on our website. From bestsellers to classics, enjoy seamless browsing, personalized recommendations, and secure purchasing. Your ultimate destination for readers seeking knowledge, adventure, and inspiration!</p>
+             </div>
+          </div>
+          <div className="w-full  md:w-1/2">Right</div>
+         
+         </div>
+         </>
+  )
+}
+
+export default Banner
